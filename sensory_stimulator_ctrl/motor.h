4415455933707a1dffca9037960bf0205cfaac2f
li-fangjie <55355555+li-fangjie@users.noreply.github.com>
2020-01-28 17:51:19 -0500

    Update motor.h
    added parameter to setup(), so input can be stored as a pointer attribute.

--- sensory_stimulator_ctrl/motor.h
+++ sensory_stimulator_ctrl/motor.h
@@ -8,12 +8,18 @@ class motor
     int output_pin_1;
     int output_pin_2;
     int output_pin_pwm;
+    float * input;
+    bool input_set = false;
 
   public:
     motor(){};
     void setup(int pin_1, int pin_2, int pin_pwm);
 
+    void setup(int pin_1, int pin_2, int pin_pwm, float * n_input);
+
     void operate(int val);
+
+    void operate();
 };
 
 
